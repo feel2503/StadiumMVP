799974a8dd54f6b23ca254fe66d1d32dde49a272
phill.kim <phill.kim@pointmobile.com>
2024-02-13 19:09:50 +0900

    tt

--- ServerSide/StadiumAmp_V2/src/main/java/com/thiscat/stadiumamp/StadiumAmpApplication.java
+++ ServerSide/StadiumAmp_V2/src/main/java/com/thiscat/stadiumamp/StadiumAmpApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StadiumAmpApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StadiumAmpApplication.class, args);
     }
